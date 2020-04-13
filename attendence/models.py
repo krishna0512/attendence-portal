207dab7152ce0c1a303ad6224bd0f9d45a8b0d6b
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2020-04-13 21:50:05 +0530

    added basic models

--- attendence/models.py
+++ attendence/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile_student',
+    )
+    roll_number = models.CharField(
+        blank=False,
+        null=False,
+        default='',
+        max_length=30,
+        unique=True,
+    )
+    year_of_enrollment = models.IntegerField(
+        blank=False,
+        null=False,
+        default=2020,
+    )
+    program = models.CharField(
+        blank=False,
+        null=False,
+        default='BTech',
+        max_length=30,
+    )
+    status = models.CharField(
+        blank=True,
+        null=False,
+        default='active',
+        max_length=30,
+    )
+    gender = models.CharField(
+        blank=False,
+        null=False,
+        default='male',
+        max_length=30,
+    )
+
+class Faculty(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile_faculty',
+    )
+    fid = models.CharField(
+        blank=False,
+        null=False,
+        default='',
+        max_length=30,
+        unique=True,
+        verbose_name='Faculty ID',
+    )
+    dept = models.CharField(
+        blank=False,
+        null=False,
+        default='',
+        max_length=50,
+        verbose_name='Department',
+    )
+    status = models.CharField(
+        blank=True,
+        null=False,
+        default='active',
+        max_length=30,
+    )
+
+class Semester(models.Model):
+    name = models.CharField(
+        max_length=30,
+        default='',
+    )
+    academic_year = models.IntegerField(
+        default=2020,
+    )
+    date_start = models.DateField(
+        null=True,
+    )
+    date_end = models.DateField(
+        null=True,
+    )
+    status = models.CharField(
+        default='active',
+        max_length=30,
+    )
