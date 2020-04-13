dcebe8dd7fb9f4536ffda96d3e2fd1949edf2c3e
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2020-04-13 21:59:17 +0530

    added changes to admin to view models

--- attendence/admin.py
+++ attendence/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from attendence.models import *
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Semester)
